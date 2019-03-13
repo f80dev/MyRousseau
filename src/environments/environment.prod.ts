25086d3f995d1f7e9ba2b3f13e41db9fcf206135
shifumix <dev@shifumix.com>
2019-03-13 09:29:29 +0100

    Version 1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   root_api:"https://rousseauautomobile.appspot.com/_ah/api/rousseau/v1",
   domain:"https://rousseauautomobile.appspot.com",
-  version:"0.1.5"
+  version:"0.1.6"
 };
